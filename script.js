1c7911755ddc1b61fb0358a381b80e7c8c553058
Divyansh Harne <divyanshsanjayharne23@gmail.com>
2023-09-17 16:25:45 +0530

    First commit, Cursor made

---
+++ script.js
@@ -0,0 +1,35 @@
+var arrow = document.querySelector("#cursor")
+var blurArrow  = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove", (dets)=>{
+  arrow.style.left = dets.x+'px';
+  arrow.style.top = dets.y+'px';
+  blurArrow.style.left = dets.x-125+'px';
+  blurArrow.style.top = dets.y-125+'px';
+})
+
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "110px",
+    scrollTrigger:{
+        trigger: "#nav", 
+        scroller: "body",
+        markers: true,
+        start: "top -11%",
+        end: "top -11%",
+        scrub:1,
+    }
+});
+
+gsap.to("#main",{
+    backgroundColor: "#000", 
+    scrollTrigger:{
+        trigger:"#main",
+        Scroller: "body",
+        start: "top -25%",
+        end: "top -70%",
+        scrub:2,
+    }
+})
